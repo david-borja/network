ac4cc4d16c969da82f78fc2e6569e1255e61ca0a
david-borja <davidborjamartinez@gmail.com>
2024-10-09 20:28:56 +0200

    feat(): create post

--- network/models.py
+++ network/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+import uuid
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.CharField(max_length=140)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="own_posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id}: {self.content} posted by {self.author}"
--- network/views.py
+++ network/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Post
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, "network/index.html", {"posts": posts})
 
 
 def login_view(request):
@@ -61,3 +63,10 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+@login_required()
+def create_post(request):
+    if request.method == 'POST':
+        new_post = request.POST["new-post"]
+        Post.objects.create(content=new_post, author=request.user)
+        return HttpResponseRedirect(reverse("index"))
