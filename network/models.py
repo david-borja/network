7f1c40efc653b7064cf5e9c2cf7e7707ebc183ec
david-borja <davidborjamartinez@gmail.com>
2024-10-18 23:34:23 +0200

    feat(): seed script

--- network/models.py
+++ network/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 import uuid
 
 class User(AbstractUser):
@@ -9,11 +10,17 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     content = models.CharField(max_length=140)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="own_posts")
-    timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return f"Post {self.id}: {self.content} posted by {self.author}"
+
+    def save(self, *args, **kwargs):
+    # If no timestamp is provided, use the current time
+        if not self.timestamp:
+            self.timestamp = timezone.now()
+        super(Post, self).save(*args, **kwargs)
 
 class Follower(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
